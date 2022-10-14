88c0f59b59f5dcc74738ce2132b83c5e026ba666
Justin Hollmer <justin.hollmer@gmail.com>
2022-10-14 12:42:01 -0500

    Cases 1, 2, and 5 complete

--- src/Main.java
+++ src/Main.java
@@ -37,45 +37,57 @@ public class Main {
             contactList.add(aleradyInTheFile.get(i));
         }
 
-
-
-        // Gives users options to choose from.
-        System.out.println("Welcome to the contacts update application, please enter one of the following options.");
-        System.out.println("1: View contacts.");
-        System.out.println("2: Add a new contact.");
-        System.out.println("3: Search a contact by name.");
-        System.out.println("4: Delete an existing contact.");
-        System.out.println("5: Exit");
-
-
-        // User enters what they would like to do, recursion used to correct user errors.
-        int userChoice = scanner.nextInt();
-        while (userChoice < 1 || userChoice > 5) {
-            System.out.println("Please enter a number between 1 and 5");
-            userChoice = scanner.nextInt();
-        }
-        // Clearing out the scanner.
-        scanner.nextLine();
-
-
-        switch (userChoice) {
-            case 1 :
-
-            case 2 :
-                System.out.println("Please enter the name of your new contact.");
-                String contactName = scanner.nextLine();
-                System.out.println("Please enter the phone number of your new contact.");
-                String contactNumber = scanner.nextLine();
-                Contact newContact = new Contact(contactName, contactNumber);
-                contactList.add(newContact.getName() + " - " + newContact.getPhoneNumber());
-            case 3 :
-
-            case 4 :
-
-            case 5 :
-
-        }
-
         System.out.println(contactList);
+
+        System.out.println("Welcome to the contacts update application");
+
+
+        int testCondition = 0;
+        while (testCondition == 0) {
+
+            // Gives users options to choose from.
+            System.out.println("Please choose an option.");
+            System.out.println("1: View contacts.");
+            System.out.println("2: Add a new contact.");
+            System.out.println("3: Search a contact by name.");
+            System.out.println("4: Delete an existing contact.");
+            System.out.println("5: Exit");
+
+            // User enters what they would like to do, recursion used to correct user errors.
+            int userChoice = scanner.nextInt();
+            while (userChoice < 1 || userChoice > 5) {
+                System.out.println("Please enter a number between 1 and 5");
+                userChoice = scanner.nextInt();
+            }
+            // Clearing out the scanner.
+            scanner.nextLine();
+
+            switch (userChoice) {
+                case 1:
+                    System.out.println("Current saved contacts: ");
+                    for (String contact : contactList) {
+                        System.out.println(contact);
+                    }
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.println("Please enter the name of your new contact.");
+                    String contactName = scanner.nextLine();
+                    System.out.println("Please enter the phone number of your new contact.");
+                    String contactNumber = scanner.nextLine();
+                    Contact newContact = new Contact(contactName, contactNumber);
+                    contactList.add(newContact.getName() + " - " + newContact.getPhoneNumber());
+                    System.out.println();
+                    break;
+                case 3:
+
+                case 4:
+
+                case 5:
+                    System.out.println("Exiting the program.");
+                    Files.write(filePath, contactList);
+                    testCondition += 1;
+            }
+        }
     }
 }
